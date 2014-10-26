01790cf768e66f07448ae0377382599f75c735f6
jdmdev <jordmax12@gmail.com>
2014-10-26 02:22:17 +0000

    upload

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,19 @@
+class WelcomeController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+
+  def contact
+  end
+
+  def features
+  end
+
+  def portfolio
+  end
+
+  def resume
+  end
+end
