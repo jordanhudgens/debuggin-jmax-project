d14675b014c51ff468c7b991de48e1a2f75a5370
jdmdev <jordmax12@gmail.com>
2014-11-06 22:18:05 +0000

    push

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,5 +1,5 @@
 class Invoice < ActiveRecord::Base
-  belongs_to :salesperson
+  belongs_to :employee
   def self.import(file)
     CSV.foreach(file.path, headers: true) do |row|
       Company.create! row.to_hash
